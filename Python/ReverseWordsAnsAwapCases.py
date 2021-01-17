ba8f70fac278387301db3244702020ccc056527a
Niyati Sinha <niyati.sinha29@gmail.com>
2021-01-18 01:27:06 +0530

    Create ReverseWordsAnsAwapCases.py

---
+++ Python/ReverseWordsAnsAwapCases.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'reverse_words_order_and_swap_cases' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING sentence as parameter.
+#
+
+def reverse_words_order_and_swap_cases(sentence):
+    # Write your code here
+    listOfWords=sentence.split()
+    #print(listOfWords)
+    listOfWords=list(reversed(listOfWords))
+    #print(listOfWords)
+    finalString=(" ".join(listOfWords)).swapcase()
+    return finalString
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    sentence = input()
+
+    result = reverse_words_order_and_swap_cases(sentence)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
